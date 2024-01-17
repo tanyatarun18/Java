6d7df8c6da6070899d036a9e1a04ed0d07fd3d34
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2024-01-17 21:46:31 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+
+public class Datatypes
+{
+    public static void main(String[] args)
+    {
+        byte a = 3; // Size - 1 bytes [can store from -128 to 127]
+        System.out.println(a);
+        int b = 4;  // 4 bytes
+        float c = 34.4;  // 4 bytes
+        long d =  79;  // 8 bytes
+        short e = 23; //2 bytes
+        char sa = 'a'; // 2 bytes
+        boolean var = true; // 1 byte
+        double da = 4432.9864987; // 8 bytes
+    }
+}

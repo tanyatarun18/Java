8668a501624ea315cac6f5de42ca029eb441d2eb
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2024-01-17 22:08:34 +0530

    Datatypes.java

--- Datatypes.java
+++ Datatypes.java
@@ -6,7 +6,7 @@ public class Datatypes
         byte a = 3; // Size - 1 bytes [can store from -128 to 127]
         System.out.println(a);
         int b = 4;  // 4 bytes
-        float c = 34.4;  // 4 bytes
+        float c = 34.4f;  // 4 bytes
         long d =  79;  // 8 bytes
         short e = 23; //2 bytes
         char sa = 'a'; // 2 bytes

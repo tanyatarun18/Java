6b2ccce9be044c60970af288e546b2bce5e84a1f
Tanya Tarun <112303448+tanyatarun18@users.noreply.github.com>
2024-01-17 23:18:09 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class Input
+{
+    public static void main(String[] args)
+    {
+        /*Scanner num = new Scanner(System.in);
+        int num1 = num.nextInt(); // for int data type
+        System.out.println(num1);
+
+        String var = num.next(); // for single word input
+        System.out.println(var);
+
+        float num2 = num.nextFloat(); // for float data type
+        System.out.println(num2);
+
+        Byte num3 = num.nextByte();  // for btye data type
+        System.out.println(num3);
+
+        String var1 = num.nextLine(); // for multiple word input or sentence
+        System.out.println(var1);
+
+        Double num4 = num.nextDouble(); // for double data type
+        System.out.println(num4);
+
+        Boolean n = num.nextBoolean(); // for boolean data type
+        System.out.println(n);
+
+        Long n1 = num.nextLong(); // for long data type
+        System.out.println(n1);
+
+        Short n2 = num.nextShort(); // for short data type
+        System.out.println(n2);*/
+
+
+        // Practice - Sum of a and b
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+    }
+}
